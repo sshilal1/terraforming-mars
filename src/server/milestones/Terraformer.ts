1370044959471a08b3cfb3a7993ff395f5b3206d
Stephen Shilale <sshilale@thomasnet.com>
2023-08-02 08:46:43 -0600

    made terraformer milestone 25 for turmoil

--- src/server/milestones/Terraformer.ts
+++ src/server/milestones/Terraformer.ts
@@ -6,10 +6,10 @@ import {Turmoil} from '../turmoil/Turmoil';
 export class Terraformer implements IMilestone {
   public readonly name = 'Terraformer';
   private terraformRating: number = 35;
-  private terraformRatingTurmoil: number = 26;
+  private terraformRatingTurmoil: number = 35;
   public readonly description;
   constructor() {
-    this.description = 'Have a terraform rating of 35 (or 26 with Turmoil.)';
+    this.description = 'Have a terraform rating of 35 (or 35 with Turmoil.)';
   }
   public getScore(player: IPlayer): number {
     return player.getTerraformRating();
